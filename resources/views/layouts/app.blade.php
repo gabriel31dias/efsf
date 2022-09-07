ca0cc8032afdf2560cc6d6cbcf51419f1159b0bb
gabriel31dias <diassilvag31@gmail.com>
2022-09-07 00:14:04 -0300

    implementing header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -161,8 +161,8 @@
               <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
                 <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
                 <div class="d-none d-xl-block ps-2">
-                  <div>{{Auth::user()->email}}</div>
-                  <div class="mt-1 small text-muted">{{Auth::user()->name}}</div>
+                  <div>{{Auth::user()->email ?? ''}}</div>
+                  <div class="mt-1 small text-muted">{{Auth::user()->name ?? ''}}</div>
                 </div>
               </a>
               <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
