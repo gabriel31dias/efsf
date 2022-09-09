9fcadc233dbb5fb053b67157792f0d64f2441483
gabriel31dias <diassilvag31@gmail.com>
2022-09-09 16:03:54 -0300

    update table country

--- database/migrations/2022_09_09_041451_create_country__table.php
+++ database/migrations/2022_09_09_041451_create_country__table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('country_', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
             $table->timestamps();
         });
     }
