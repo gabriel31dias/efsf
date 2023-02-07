a376330913343b343c4799ddc48b8ed2d6d08306
gabriel31dias <diassilvag31@gmail.com>
2023-02-07 14:23:22 -0300

    adjustments

--- resources/views/livewire/citizen/citizen-index.blade.php
+++ resources/views/livewire/citizen/citizen-index.blade.php
@@ -391,7 +391,7 @@ role="dialog"  aria-hidden="true">
                          <line x1="12" y1="5" x2="12" y2="19"/>
                          <line x1="5" y1="12" x2="19" y2="12"/>
                       </svg>
-                      Salvar
+                      Finalizar
                    </a>
                    @endif
                 </div>
